acb154a6596d06b1cbf56165f2e89055c8656791
Bruno Lima <brunolimadevelopment@gmail.com>
2023-04-01 14:58:27 -0300

    Criando path static /public

---
+++ aula10/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+/**
+ * Importa as rotas da aplicação.
+ */
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true })); // Para que possamos ver a body da requisição do tipo post.
+app.use(express.static(path.resolve(__dirname, 'public'))); // Conteúdo estático
+
+/**
+ * Renderizando arquivos da pasta views
+ */
+app.set('views', path.resolve(__dirname, 'src', 'views')); // Path absoluto de views.
+app.set('view engine', 'ejs'); // Para renderizar for, if e outros nos views.
+
+app.use(routes);
+
+app.listen(3000, () => {
+    console.log('Acessar http://localhost:3000');
+    console.log('Servidor executando na porta 3000');
+});
